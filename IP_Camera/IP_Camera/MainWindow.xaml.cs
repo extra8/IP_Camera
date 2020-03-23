be29b7e50cb63bf8ecb03ed85595940927d5f3e0
Andrei Petrila <petrila.ac@gmail.com>
2020-03-23 17:01:22 +0200

    App v2

--- IP_Camera/IP_Camera/MainWindow.xaml.cs
+++ IP_Camera/IP_Camera/MainWindow.xaml.cs
@@ -17,8 +17,6 @@ namespace IP_Camera
         public MainWindow()
         {
             InitializeComponent();
-            stream = new MJPEGStream("http://192.168.1.9:4747/video");
-            stream.NewFrame += StreamNewFrame;
         }
 
         void StreamNewFrame(object sender, NewFrameEventArgs eventArgs)
@@ -33,7 +31,6 @@ namespace IP_Camera
             }
             catch(Exception)
             {
-
             }
         }
 
@@ -49,15 +46,43 @@ namespace IP_Camera
             return image;
         }
 
+
+        // http://192.168.1.9:4747/video
         private void ConnectButton_Clicked(object sender, RoutedEventArgs e)
         {
+            var streamSource = ProcessCameraUrl(CameraUrlTextBox.Text);
+
+            stream = new MJPEGStream(streamSource);
+            stream.NewFrame += StreamNewFrame;
+
             stream.Start();
+        }
+
+        private string ProcessCameraUrl(string cameraUrl)
+        {
+            var lowerCameraUrl = cameraUrl.ToLower();
+            if (lowerCameraUrl.Contains("http://") ||  lowerCameraUrl.Contains("https://"))
+            {
+                return cameraUrl;
+            }
+            else
+            {
+                return $"http://{cameraUrl}";
+            }
         }
 
         private void DisconnectButton_Clicked(object sender, RoutedEventArgs e)
         {
-            stream.Stop();
-            VideoDisplay.Source = null;
+            try
+            {
+                stream.Stop();
+                stream = null;
+                VideoDisplay.Source = null;
+            }
+            catch(NullReferenceException)
+            {
+
+            }
         }
     }
 }
